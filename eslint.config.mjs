4312a467d8669e2d812e6a65f9537d5f8b6e68d9
AnamSadat <anamsadat3@gmail.com>
2025-07-12 21:26:38 +0700

    setup eslint for clean code

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'space-before-blocks': ['error', 'always'],
+      'space-in-parens': ['error', 'never'],
+      'space-infix-ops': 'error',
+      'space-unary-ops': ['error', { words: true, nonwords: false }],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'block-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'comma-spacing': ['error', { before: false, after: true }],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'if' },
+        { blankLine: 'always', prev: '*', next: 'for' },
+        { blankLine: 'always', prev: '*', next: 'while' },
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] }
+      ]
+    },
+  },
+];
+
+export default eslintConfig;
